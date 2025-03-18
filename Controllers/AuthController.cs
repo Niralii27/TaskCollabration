2a57275cd0932c944abf153a0366570e9a4dbadb
Niralii27 <akbarinirali27@gmail.com>
2025-03-18 10:23:57 +0530

    Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -12,5 +12,39 @@ namespace TaskCollabration.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AuthModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+
+                if (user != null)
+                {
+                    HttpContext.Session.SetString("UserRole", user.Role);
+
+                    switch (user.Role)
+                    {
+                        case "Admin":
+                            return RedirectToAction("Dashboard", "Admin");
+                        case "Team Leader":
+                            return RedirectToAction("Dashboard", "TeamLeader");
+                        case "User":
+                            return RedirectToAction("Dashboard", "User");
+                        default:
+                            return RedirectToAction("Login", "Account");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid email or password");
+                }
+            }
+
+            return View(model);
+        }
+
     }
 }
